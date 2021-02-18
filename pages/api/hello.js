54fd37fad283d58d0ab357bfab6ca06e3b16ab31
YutaGoto <you.goto.510@gmail.com>
2021-02-18 21:28:18 +0900

    :heart: :world_map: What3Idols

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {BigQuery} from '@google-cloud/bigquery'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+const credential = JSON.parse(
+  Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64').toString()
+);
+
+const bigquery = new BigQuery({
+  projectId: 'imasparql-slack',
+  credentials: credential,
+});
+
+const hello = async (req, res) => {
+  const idols = req.body
+  const query = `select lat, lng
+  from \`imasparql-slack.what3idols.maps\`
+  where idols = "${idols}";`
+
+  const options = {
+    query: query,
+    jobTimeoutMs: 2000,
+  };
+
+  const [job] = await bigquery.createQueryJob(options);
+  const [rows] = await job.getQueryResults();
+
+  if (rows.length === 0){
+    return res.status(404)
+  } else if (rows.length > 0){
+    return res.status(200).json(rows[0])
+  } else {
+    return res.status(404)
+  }
 }
+
+export default hello
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,154 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Container, Section, Content, Columns, Heading, Navbar, Notification, Button, Footer, Loader, Form } from 'react-bulma-components'
+import idols from '../utils/idols.json'
 
-export default function Home() {
+const Home = () => {
+  const [selectedIdols, setSelectedIdols] = useState({
+    idol1: "0",
+    idol2: "0",
+    idol3: "0",
+  })
+  const [mapUrl, setMapUrl] = useState("https://maps.google.co.jp/maps?output=embed&q=35.685261046859836,139.75277829434054&z=13")
+  const [loading, setLoading] = useState(false)
+  const [showNotification, setShowNotification] = useState(false)
+  const { Field, Select, Label, Control } = Form
+
+  const onChange = (e) => {
+    setSelectedIdols({
+      ...selectedIdols, [e.target.name]: e.target.value
+    })
+  }
+
+  const onClick = async () =>{
+    setShowNotification(false)
+    const arrayIdols = [parseInt(selectedIdols.idol1), parseInt(selectedIdols.idol2), parseInt(selectedIdols.idol3)].sort((a, b) => {
+      if (a>b) return 1
+      if (a<b) return -1
+      return 0
+    })
+
+    if (arrayIdols[0] == arrayIdols[1] || arrayIdols[1] == arrayIdols[2]) {
+      setShowNotification(true)
+      return
+    }
+
+    if (arrayIdols.includes(0)) {
+      setShowNotification(true)
+      return
+    }
+    setLoading(true)
+    await fetch('/api/hello', {
+      method: 'POST',
+      body: arrayIdols.join(",")
+    }).then((res) => {
+      setLoading(false)
+      if(res.ok)
+        return res.json()
+      else
+        throw res
+    }).then((json) => {
+      const data = json
+      if (data == undefined) return
+      setMapUrl(`https://maps.google.co.jp/maps?output=embed&q=${data.lat},${data.lng}&z=13`)
+    }).catch((err) => {
+      console.log(err)
+      setLoading(false)
+    })
+  }
+
+  const onCloseButtonClick = () => setShowNotification(false)
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar>
+        <Navbar.Brand>
+          <Navbar.Item renderAs="a" href="#">
+            <Heading>What3Idols</Heading>
+          </Navbar.Item>
+        </Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Section>
+          { showNotification ?
+            <Notification>
+              3人別々のアイドルを選択してください
+              <Button remove onClick={onCloseButtonClick} />
+            </Notification>
+            : null
+          }
+          <Columns>
+            <Columns.Column>
+              <Field>
+                <Label>アイドル</Label>
+                <Control>
+                  <Select onChange={onChange} value={selectedIdols.idol1} name="idol1" >
+                    {idols.idols.map( idol => <option key={idol.id} value={idol.id}>{idol.label}</option>)}
+                  </Select>
+                </Control>
+              </Field>
+            </Columns.Column>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <Columns.Column>
+              <Field>
+                <Label>アイドル</Label>
+                <Control>
+                  <Select onChange={onChange} value={selectedIdols.idol2} name="idol2" >
+                    {idols.idols.map( idol => <option key={idol.id} value={idol.id}>{idol.label}</option>)}
+                  </Select>
+                </Control>
+              </Field>
+            </Columns.Column>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <Columns.Column>
+              <Field>
+                <Label>アイドル</Label>
+                <Control>
+                  <Select onChange={onChange} value={selectedIdols.idol3} name="idol3" >
+                    {idols.idols.map( idol => <option key={idol.id} value={idol.id}>{idol.label}</option>)}
+                  </Select>
+                </Control>
+              </Field>
+            </Columns.Column>
+          </Columns>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Button.Group>
+            <Button color="primary" onClick={onClick} >What3Idols!!!</Button>
+            { loading ? <Loader /> : null }
+          </Button.Group>
+        </Section>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <Section>
+        <figure className="image is-16by9">
+          {mapUrl ? <iframe className="has-ratio" width="640" height="360" src={mapUrl}></iframe> : null}
+        </figure>
+        </Section>
+      </Container>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Footer>
+        <Container>
+          <Content style={{ textAlign: 'center' }}>
+            <Columns>
+              <Columns.Column size={9}>
+                <p>
+                  <strong>What 3 Idols</strong> by <a href="https://github.com/YutaGoto">Yuta Goto</a>.
+                </p>
+              </Columns.Column>
+              <Columns.Column size={3}>
+                <a href="https://bulma.io">
+                  <img src="/made-with-bulma.png" alt="Made with Bulma" width="256" height="48" />
+                </a>
+              </Columns.Column>
+            </Columns>
+          </Content>
+        </Container>
+      </Footer>
+    </>
   )
 }
+
+export default Home
