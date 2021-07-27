db21b38ba70c9a5d38ec66987fc162524a6a7ccc
YutaGoto <you.goto.510@gmail.com>
2021-07-27 22:04:43 +0900

    実行権限

--- scripts/should-skip-deploy.sh
+++ scripts/should-skip-deploy.sh
@@ -2,9 +2,9 @@
 
 # Does the branch name start with "dependabot/"
 if [[ "$VERCEL_GIT_COMMIT_REF" = dependabot/* ]] ; then
-  echo "should-skip-deploy >> Skipping deploy!"
+  echo "should-skip-deploy -> Skipping deploy!"
   exit 0;
 else
-  echo "should-ski-deploy >> Proceeding with deploy."
+  echo "should-ski-deploy -> Proceeding with deploy."
   exit 1;
 fi
