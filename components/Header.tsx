10ec686a45d72e565f4f7589802b2ccc12caed69
YutaGoto <you.goto.510@gmail.com>
2021-11-29 00:59:03 +0900

    update logo color

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,26 +6,26 @@ const Header: VFC = () => {
   const [openMenu, setOpenMenu] = useState<boolean>(false)
 
   return (
-      <Navbar active={openMenu} transparent={false}>
-        <Navbar.Brand>
-          <Navbar.Item renderAs="a" href="#">
-            <Heading>
-              <Image src="/logo.svg" alt="What3Idols" width={250} height={50} />
-            </Heading>
+    <Navbar active={openMenu} transparent={false}>
+      <Navbar.Brand>
+        <Navbar.Item renderAs="a" href="#">
+          <Heading>
+            <Image src="/logo.svg" alt="What3Idols" width={250} height={50} />
+          </Heading>
+        </Navbar.Item>
+        <Navbar.Burger onClick={() => {setOpenMenu(!openMenu)}} />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Container>
+          <Navbar.Item renderAs="a" href="/">
+            <>Idols to Map</>
           </Navbar.Item>
-          <Navbar.Burger onClick={() => {setOpenMenu(!openMenu)}} />
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Container>
-            <Navbar.Item renderAs="a" href="/">
-              <>Idols to Map</>
-            </Navbar.Item>
-            <Navbar.Item renderAs="a" href="/maps">
-              <>Map to Idols</>
-            </Navbar.Item>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+          <Navbar.Item renderAs="a" href="/maps">
+            <>Map to Idols</>
+          </Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
   )
 }
 
