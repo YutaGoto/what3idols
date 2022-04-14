02961af946a1d0896756a4885ee745ed1d178077
YutaGoto <you.goto.510@gmail.com>
2022-04-14 20:39:41 +0900

    :rocket: noindex対応

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head';
 
 interface Props {
   description: string;
+  noindex?: boolean;
+  nofollow?: boolean;
 }
 
-const Meta: VFC<Props> = ({ description }) => {
+const Meta: VFC<Props> = ({ description, noindex = false, nofollow= false }) => {
   return (
     <Head>
       <meta property="og:title" content="What3Idols" />
       <meta property="og:description" content={description} />
+      {noindex && <meta name="robots" content="noindex" />}
+      {nofollow && <meta name="robots" content="nofollow" />}
       <title>What3Idols!</title>
       <link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
