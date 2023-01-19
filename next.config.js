c2614dce18733737811f8e72a58fb12b3075793c
YutaGoto <you.goto.510@gmail.com>
2023-01-19 21:37:10 +0900

    :wrench: appDirの設定

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     GOOGLE_MAPS_MAP_ID: process.env.GOOGLE_MAPS_MAP_ID,
     PROJECT_ID: process.env.PROJECT_ID,
   },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = nextConfig;
