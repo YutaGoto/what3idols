33a5bd406b5c4cbf4f78bb97ab21449e61f8830f
YutaGoto <you.goto.510@gmail.com>
2023-04-22 01:37:15 +0900

    :lipstick: add Pane container

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@
 
 import { ErrorMessage } from '@hookform/error-message';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
-import React, { ReactElement, useState } from 'react';
-
-import { Button, SelectField, toaster } from 'evergreen-ui';
+import { ReactElement, useState } from 'react';
+import { Pane, Button, SelectField, toaster } from 'evergreen-ui';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 import { Meta, Layout } from '../components';
-import { LatLng, SelectedIdols, NotificationToast } from '../types/Type';
+import { LatLng, SelectedIdols } from '../types/Type';
 import idols from '../utils/idols.json';
 
 const containerStyle = {
@@ -80,76 +79,103 @@ const Home = (): ReactElement => {
     <>
       <Meta description="アイドルを3人選んで位置を特定しましょう" />
       <Layout>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <>
-            <Controller
-              name="idol1"
-              control={control}
-              rules={{ required: 'アイドルを選択してください', validate: isUnique }}
-              render={({ field }) => (
-                <SelectField label="アイドル" {...field}>
-                  {idols.idols.map((idol) => (
-                    <option key={idol.id} value={idol.label}>
-                      {idol.label}
-                    </option>
-                  ))}
-                </SelectField>
-              )}
-            />
-            <ErrorMessage errors={errors} name="idol1" render={({ message }) => <>{message}</>} />
-
-            <Controller
-              name="idol2"
-              control={control}
-              rules={{ required: 'アイドルを選択してください', validate: isUnique }}
-              render={({ field }) => (
-                <SelectField label="アイドル" {...field}>
-                  {idols.idols.map((idol) => (
-                    <option key={idol.id} value={idol.label}>
-                      {idol.label}
-                    </option>
-                  ))}
-                </SelectField>
-              )}
-            />
-            <ErrorMessage errors={errors} name="idol2" render={({ message }) => <>{message}</>} />
-
-            <Controller
-              name="idol3"
-              control={control}
-              rules={{ required: 'アイドルを選択してください', validate: isUnique }}
-              render={({ field }) => (
-                <SelectField label="アイドル" {...field}>
-                  {idols.idols.map((idol) => (
-                    <option key={idol.id} value={idol.label}>
-                      {idol.label}
-                    </option>
-                  ))}
-                </SelectField>
-              )}
-            />
-            <ErrorMessage errors={errors} name="idol3" render={({ message }) => <>{message}</>} />
-
-            <Button color="primary" disabled={loading} className="has-text-white">
-              What3Idols!!!
-            </Button>
-          </>
-        </form>
-
-        <>
-          {isLoaded && (
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              zoom={10}
-              center={pinLatLng || initPosition}
-              options={{
-                mapId: process.env.GOOGLE_MAPS_MAP_ID,
-              }}
+        <Pane marginX="auto" width={960}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Pane
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+              marginY={20}
             >
-              <Marker position={pinLatLng} />
-            </GoogleMap>
-          )}
-        </>
+              <Pane>
+                <Controller
+                  name="idol1"
+                  control={control}
+                  rules={{ required: 'アイドルを選択してください', validate: isUnique }}
+                  render={({ field }) => (
+                    <SelectField label="アイドル" {...field}>
+                      {idols.idols.map((idol) => (
+                        <option key={idol.id} value={idol.label}>
+                          {idol.label}
+                        </option>
+                      ))}
+                    </SelectField>
+                  )}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="idol1"
+                  render={({ message }) => <>{message}</>}
+                />
+              </Pane>
+              <Pane>
+                <Controller
+                  name="idol2"
+                  control={control}
+                  rules={{ required: 'アイドルを選択してください', validate: isUnique }}
+                  render={({ field }) => (
+                    <SelectField label="アイドル" {...field}>
+                      {idols.idols.map((idol) => (
+                        <option key={idol.id} value={idol.label}>
+                          {idol.label}
+                        </option>
+                      ))}
+                    </SelectField>
+                  )}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="idol2"
+                  render={({ message }) => <>{message}</>}
+                />
+              </Pane>
+
+              <Pane>
+                <Controller
+                  name="idol3"
+                  control={control}
+                  rules={{ required: 'アイドルを選択してください', validate: isUnique }}
+                  render={({ field }) => (
+                    <SelectField label="アイドル" {...field}>
+                      {idols.idols.map((idol) => (
+                        <option key={idol.id} value={idol.label}>
+                          {idol.label}
+                        </option>
+                      ))}
+                    </SelectField>
+                  )}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="idol3"
+                  render={({ message }) => <>{message}</>}
+                />
+              </Pane>
+            </Pane>
+
+            <Pane>
+              <Button appearance="primary" isLoading={loading}>
+                What3Idols!!!
+              </Button>
+            </Pane>
+          </form>
+
+          <Pane marginY={10}>
+            {isLoaded && (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                zoom={10}
+                center={pinLatLng || initPosition}
+                options={{
+                  mapId: process.env.GOOGLE_MAPS_MAP_ID,
+                }}
+              >
+                <Marker position={pinLatLng} />
+              </GoogleMap>
+            )}
+          </Pane>
+        </Pane>
       </Layout>
     </>
   );
